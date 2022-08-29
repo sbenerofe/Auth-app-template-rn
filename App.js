fb578e02b8765454a7cae3195b38fc705306c588
Steven Benerofe <sbenerofe@gmail.com>
2022-08-28 21:15:31 -0700

    update auth token

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import AppNavigator from './navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <AppNavigator />
+  );
+}
+
+const styles = StyleSheet.create({
+});
